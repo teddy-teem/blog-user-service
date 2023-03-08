8232fe8930d918ac250fe6f90fcec16579d0d713
Jahid Hasan <teddyteem@gmail.com>
2023-03-08 22:24:00 +0600

    Update userController.js

--- controller/userController.js
+++ controller/userController.js
@@ -2,45 +2,57 @@ const ddb = require("../model/ddb");
 const { v4: uuidv4 } = require("uuid");
 
 exports.getUserByContactId = async ctx => {
-  const { contactId } = ctx.request.params;
-  const data = await ddb.getUser(contactId);
-  if (!Object.keys(data).length) {
-    throw { msg: "User not found" };
+  try {
+    const { contactId } = ctx.request.params;
+    const data = await ddb.getUser(contactId);
+    if (!Object.keys(data).length) {
+      throw { msg: "User not found" };
+    }
+    ctx.body = {
+      msg: "Successfully fetched userDetails",
+      data: data.Item
+    };
+  } catch(error) {
+    ctx.body = { status: "Failed", error: JSON.stringify(error, null, 2) };
   }
-  ctx.body = {
-    msg: "Successfully fetched userDetails",
-    data: data.Item
-  };
 };
 exports.getUserByEmail = async ctx => {
-  const email = ctx.request.body.email;
-  const data = await ddb.getUserByEmail(email);
-  if (!Object.keys(data).length) {
-    throw { msg: "User not found" };
+  try {
+    const email = ctx.request.body.email;
+    const data = await ddb.getUserByEmail(email);
+    if (!Object.keys(data).length) {
+      throw { msg: "User not found" };
+    }
+    ctx.body = {
+      msg: "Successfully fetched userDetails",
+      data: data
+    };
+  } catch(error) {
+    ctx.body = { status: "Failed", error: JSON.stringify(error, null, 2) };
   }
-  ctx.body = {
-    msg: "Successfully fetched userDetails",
-    data: data
-  };
 };
 exports.createUser = async ctx => {
-  const userInfo = ctx.request.body.userInfo;
-  if (
-    !userInfo.firstName ||
-    !userInfo.lastName ||
-    !userInfo.email ||
-    !userInfo.mobile
-  ) {
-    return (ctx.body = {
-      status: "failed",
-      msg: "Some required field are missing"
-    });
+  try {
+    const userInfo = ctx.request.body.userInfo;
+    if (
+      !userInfo.firstName ||
+      !userInfo.lastName ||
+      !userInfo.email ||
+      !userInfo.mobile
+    ) {
+      return (ctx.body = {
+        status: "failed",
+        msg: "Some required field are missing"
+      });
+    }
+    await ddb.createUser(userInfo);
+    ctx.body = {
+      status: "success",
+      msg: "Successfully created user"
+    };
+  } catch(error) {
+       ctx.body = { status: "Failed", error: JSON.stringify(error, null, 2) };
   }
-  await ddb.createUser(userInfo);
-  ctx.body = {
-    status: "success",
-    msg: "Successfully created user"
-  };
 };
 
 exports.deleteBlogUser = async ctx => {
